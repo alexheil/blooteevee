9fc549f1b63be207f50fde55b7068685b8ff0ca8
alexheil <aheil4250@gmail.com>
2018-09-11 19:09:50 -0400

    origin

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :current_password, :remember_me, :customer_id, :plan_id, :product_id, :purchase_email, :refund_email, :subscription_email, :review_email, :comment_email, :message_email, :course_email, :post_email]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+
+  devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations",  unlocks: "users/unlocks"}
+
+  devise_scope :user do
+    get 'sign_out', to: 'users/sessions#destroy', path: 'sign-out'
+    # get 'user_edit_plan', to: 'users/registrations#edit_plan', path: 'users/:id/edit-plan'
+    # patch 'user_update_plan', to: 'users/registrations#update_plan', path: 'users/:id/update-plan'
+  end
+
+
+end
