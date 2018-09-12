bf0e5f902a712a9a4d83f25f9a461ded7dc22148
alexheil <aheil4250@gmail.com>
2018-09-12 00:18:49 -0400

    routes for members

---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,18 @@
+class Users::UsersController < ApplicationRecord
+
+  def show
+  end
+
+  def edit_source
+  end
+
+  def update_source
+  end
+
+  def edit_plan
+  end
+
+  def update_plan
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,25 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
 
+  get 'become-member' => 'static_pages#home'
+
   devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations",  unlocks: "users/unlocks"}
 
   devise_scope :user do
     get 'sign_out', to: 'users/sessions#destroy', path: 'sign-out'
-    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_in', to: 'users/sessions#new', path: 'sign-in'
     get 'register', to: 'users/registrations#new'
-    # get 'user_edit_plan', to: 'users/registrations#edit_plan', path: 'users/:id/edit-plan'
-    # patch 'user_update_plan', to: 'users/registrations#update_plan', path: 'users/:id/update-plan'
   end
+
+  resources :users, controller: 'users/users', only: :show do
+    resource :profile, controller: 'users/profiles', only: [:edit, :update]
+    resource :merchant, controller: 'users/merchants', only: [:new, :create, :edit, :update]
+    get 'edit_source', to: 'users#edit_source', path: 'users/:id/edit-source'
+    patch 'update_source', to: 'users#update_source', path: 'users/:id/update-source'
+    get 'edit_plan', to: 'users#edit_plan', path: 'users/:id/edit-plan'
+    patch 'update_plan', to: 'users#update_plan', path: 'users/:id/update-plan'
+  end
+
 
 
 end
