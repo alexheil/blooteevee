32745698715f8828b2e8f22fb6f7389e3ab7624c
alexheil <aheil4250@gmail.com>
2018-09-15 02:17:32 -0400

    header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,34 @@
 //= require turbolinks
 //= require_tree .
 
+var menuToggle = function() {
+
+  $('#menu-click').click(function() {
+    $('#menu-container').fadeIn(300);
+    $('.back').fadeIn(300);
+    $(".wrapper").addClass("blur");
+    $('.background').addClass('blur');
+  });
+
+  $('.back').click(function() {
+    $('#menu-container').fadeOut(300);
+    $('.back').fadeOut(300);
+    $(".wrapper").removeClass("blur");
+    $('.background').removeClass('blur');
+  });
+
+  $('#close-menu').click(function() {
+    $('#menu-container').fadeOut(300);
+    $('.back').fadeOut(300);
+    $(".wrapper").removeClass("blur");
+    $('.background').removeClass('blur');
+  });
+
+};
+
+$(document).ready(menuToggle);
+$(document).on('turbolinks:load', menuToggle);
+
 var flashTimeout = function() {
 
   setTimeout(function() {
