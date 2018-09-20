c96861bc3f5f1249a3fffa7d4fd466fa9e06b622
alexheil <aheil4250@gmail.com>
2018-09-19 23:19:00 -0400

    share

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,6 +13,9 @@ class StaticPagesController < ApplicationController
     if user_signed_in?
       Stripe.api_key = "sk_test_ECd3gjeIEDsGkySmF8FQOC5i"
       @customer = Stripe::Customer.retrieve(current_user.customer_id)
+      if current_user.merchant.present?
+        @merchant = Stripe::Account.retrieve(current_user.merchant.stripe_id)
+      end
     end
   end
   
