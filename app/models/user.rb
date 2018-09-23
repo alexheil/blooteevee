8d961f840cb7f71c7d2e380e46683fee91833009
alexheil <aheil4250@gmail.com>
2018-09-23 18:41:39 -0400

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ApplicationRecord
 
   def feed
     subscribing_ids = "SELECT subscribed_id FROM relationships WHERE  subscriber_id = :user_id"
-    # Video.where("user_id IN (#{subscribing_ids}) OR user_id = :user_id", user_id: id)
+    Video.where("user_id IN (#{subscribing_ids}) OR user_id = :user_id", user_id: id)
   end
 
   private
